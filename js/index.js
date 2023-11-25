668d62b436a5d9f54379b99678a0352dfb68566f
HachimEnnaji <142834964+HachimEnnaji@users.noreply.github.com>
2023-11-25 13:59:02 +0100

    Add files via upload
    scope optimization with let variables outside the function

--- js/index.js
+++ js/index.js
@@ -1,16 +1,14 @@
 window.onscroll = function () {
   colorIt();
 };
+let change = document.querySelector(".container");
+let button = document.querySelector("ul li:last-child a");
 function colorIt() {
   if (document.documentElement.scrollTop > 400) {
-    let change = document.querySelector(".container");
-    let button = document.querySelector("ul li:last-child a");
     change.classList.add("changeColorway");
     button.style.backgroundColor = "#1a8917";
     button.style.transition = "650ms";
   } else {
-    let change = document.querySelector(".container ");
-    let button = document.querySelector("ul li:last-child a");
     change.classList.remove("changeColorway");
     button.style.backgroundColor = "#000000";
     button.style.transition = "650ms";
